23ee123c0725b4a9f2100605a6ec609932030dfe
sergei3141 <sergei3141@mail.ru>
2023-12-28 23:13:45 +0500

    token expired change

--- config/auth.php
+++ config/auth.php
@@ -98,8 +98,8 @@ return [
         'users' => [
             'provider' => 'users',
             'table' => 'password_reset_tokens',
-            'expire' => 60,
-            'throttle' => 60,
+            'expire' => 360,
+            'throttle' => 360,
         ],
     ],
 
